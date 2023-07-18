881461ef488041818b9e65f6777107ac5c4c07aa
KryssNa <krishna.kryss@gmail.com>
2023-07-18 19:31:19 +0545

    calling All user details from main frame

--- src/Task7/Main.java
+++ src/Task7/Main.java
@@ -4,10 +4,11 @@ import Task7.View.AllUserFrame;
 
 import javax.swing.*;
 
+// This is the main class that runs the program
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            AllUserFrame frame = new AllUserFrame();
+            AllUserFrame frame = new AllUserFrame(); // this is the main frame that contains all the users
             frame.setVisible(true);
         });
     }
